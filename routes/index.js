c68500743aab6717ebade6deec2347650f9f4f6f
Nivesh Singh Chauhan <nivesh1998@gmail.com>
2020-11-15 23:49:07 +0530

    added orders route to root

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const productRoutes = require('./product');
+const orderRoutes = require('./order');
 
 
 router.use('/products', productRoutes);
+router.use('/orders', orderRoutes);
 
 
 module.exports = router;
