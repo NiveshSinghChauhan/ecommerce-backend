90f87cc4b6e82f5e4b250bdbc23951d87780e30e
Nivesh Singh Chauhan <nivesh1998@gmail.com>
2020-11-15 20:33:49 +0530

    Added create product route and list all products route

--- routes/product/index.js
+++ routes/product/index.js
@@ -1,8 +1,46 @@
-const router = require('express').Router();
+const { request } = require('express');
+const productModel = require('../../models/product');
 
+const router = require('express').Router();
+const ProductModel = require('../../models/product');
+
+
+router.post('/', (req, res) => {
+    try {
+        const productData = req.body;
+
+        const createdProduct = await new ProductModel(productData).save();
+
+        res.status(201).json({
+            product_id: createdProduct._id,
+            timestamp: createdProduct.timestamp
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ errorMessage: error.message });
+    }
+})
 
 router.get('/', (req, res) => {
-    
+    try {
+        const { projection } = req.query;
+
+        const products = await ProductModel.aggregate([
+            {
+                $match: {}
+            },
+            {
+                $project: projection.reduce((acc, curr) => Object.assign(acc, { [curr]: 1 }), { _id: 1 })
+            }
+        ]).exec();
+
+        res.status(200).json({ products: products.data });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ errorMessage: error.message });
+    }
 })
 
 
