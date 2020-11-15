c57768318d5e0425ab21d5b489b52ab84d716efa
Nivesh Singh Chauhan <nivesh1998@gmail.com>
2020-11-15 23:48:53 +0530

    created the create order route

---
+++ routes/order/index.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+
+const OrderModel = require('../../models/order');
+const ProductModel = require('../../models/product');
+
+router.get('/create', (req, res) => {
+    try {
+        const { product_id, quantity } = req.body;
+        const { _id: customer_id } = req.AUTH;
+
+        // getting the product that is ordered
+        const product = await ProductModel.findOne({ _id: product_id }).select('selling_price stock name').exec();
+
+        // checking the if product is in stock or not
+        if (product.stock === 0) {
+            res.status(400).json({ errorMessage: 'Product out of stock' });
+            return;
+        } else if (product.stock < quantity) {
+            res.status(400).json({ errorMessage: 'Stock of the product is not sufficient to fullfill the order' });
+            return;
+        }
+
+        // creating the order
+        const createdOrder = await new OrderModel({
+            customer_id,
+            shipping: 10,
+            currency: 'INR',
+            status: 'processing',
+            product: {
+                product_id,
+                name: product.name,
+                price: product.selling_price,
+                quantity
+            }
+        }).save();
+
+        // updating the stock of the product
+        await ProductModel.updateOne({ _id: product_id }, { $inc: { stock: (-1 * quantity) } }).exec();
+
+        res.status(201).json({
+            order_id: createdOrder._id,
+            timestamp: createdOrder.timestamp
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ errorMessage: error.message });
+    }
+})
+
+module.exports = router;
