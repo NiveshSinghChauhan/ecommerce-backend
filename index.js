bf4fdcdaa7be7b46618554b078597a465ec80224
Nivesh Singh Chauhan <nivesh1998@gmail.com>
2020-11-15 20:34:17 +0530

    Connect to mongodb using mongoose

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const { connect: connectDB } = require('mongoose');
 const PORT = 3000;
-
+const DB_URI = 'mongodb://localhost:27017/demoEcommerce'
 
 const server = async () => {
+    await connectDB(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
     const app = express();
 
     app.listen(PORT, () => {
