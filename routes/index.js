14a6bcebe09d1eef42f6007a18211304e21a0b26
Nivesh Singh Chauhan <nivesh1998@gmail.com>
2020-11-21 23:43:11 +0530

    Added customer route to index route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 
 const productRoutes = require('./product');
+const customerRoutes = require('./customer');
 const orderRoutes = require('./order');
 const cartItemRoutes = require('./cart-item');
 const authorization = require('../middleware/authorization');
 
+router.use('/customers', customerRoutes);
 router.use('/products', productRoutes);
 router.use('/orders', orderRoutes);
 router.use('/cart-items', authorization, cartItemRoutes);
